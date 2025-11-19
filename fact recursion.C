d498638826f4a07b4011c59880dde9b0d7b1caca
shambhavicoder77 <shambhaviraj590@gmail.com>
2025-11-19 16:12:00 +0530

    Create fact recursion.C

---
+++ fact recursion.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+long long factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("Factorial of %d = %lld\n", n, factorial(n));
+    }
+
+    return 0;
+}
+o/p
+Enter a number: 7
+Factorial of 7 = 5040
